f4d3ae0a7d05e118c79a3fb5799c4253d798f87e
Rory Eagan <rory.eagan@gmail.com>
2018-03-16 16:31:10 -0400

    On first open user sent to setting page.

--- client/components/App.js
+++ client/components/App.js
@@ -6,24 +6,19 @@ import SettingsScreen from './SettingsScreen';
 import {Button, View, Text } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 
-
 const RootStack = StackNavigator(
   {
     LoginScreen: {
       screen: LoginScreen,
-      headerLeft: null,
     },
     AlarmsScreen: {
       screen: AlarmsScreen,
-      headerLeft: null,
     },
     SettingsScreen: {
       screen: SettingsScreen,
-      headerLeft: null,
     },
     AddScreen: {
       screen: AddScreen,
-      headerLeft: null,
     }
   },
   {
@@ -36,3 +31,4 @@ export default class App extends React.Component {
     return <RootStack />;
   }
 }
+
