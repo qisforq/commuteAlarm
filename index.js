7fa4cebe2bf15e710d510b77492e17f1ba1bc941
Danny <danny@Dannys-MacBook-Pro.local>
2018-04-04 16:42:44 -0400

    fix

--- index.js
+++ index.js
@@ -1,22 +1,12 @@
 import * as React from 'react';
 import { AppRegistry } from 'react-native';
 import App from './client/components/App';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
 
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: 'tomato',
-    accent: 'yellow',
-  },
-};
 
 export default function Main() {
   return (
-    <PaperProvider theme={theme}>
+
       <App />
-    </PaperProvider>
   );
 }
 
